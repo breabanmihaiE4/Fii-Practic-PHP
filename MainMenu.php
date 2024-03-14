e6699569e7a30f3d5e3ab1046b7db5db611e2dd5
Breaban Mihai <mihai.breaban@yahoo.com>
2024-03-14 19:46:39 +0200

    Initialize project
    Added css and PHP for main menu

---
+++ MainMenu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/mainMenu.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="menu">
+        <ul class="menu-content">
+            <li><a href="#"><span class="material-symbols-outlined">1</span><span>Exercise 1</span></a></li>
+            <li><a href="#"><span class="material-symbols-outlined">2</span><span>Exercise 2</span></a></li>
+            <li><a href="#"><span class="material-symbols-outlined">3</span><span>Exercise 3</span></a></li>
+            <li><a href="#"><span class="material-symbols-outlined">4</span><span>Exercise 4</span></a></li>
+        </ul>
+    </div>
+
+    <div class = "card">
+    </div>
+</div>  
+</body>
+</html>
